bf0c52e667457c84a70c88e49745ea1f923d5fa8
amalkp123-cpu <amalkp894@gmail.com>
2025-08-08 01:03:43 +0530

    Your message

--- src/About.jsx
+++ src/About.jsx
@@ -1,9 +1,9 @@
 import React from 'react'
-import  './About.css'
+import './About.css'
 import rect from './assets/Rectangle.jpg'
-import rect1 from './assets/Rectangle 2.avif'
-import rect2 from './assets/Rectangle 3.avif'
-import rect3 from './assets/Rectangle 220.avif'
+import rect1 from './assets/rectangle 2.avif'
+import rect2 from './assets/rectangle 3.avif'
+import rect3 from './assets/rectangle 220.avif'
 import vector from './assets/vector.svg'
 
 const About = () => {
@@ -11,59 +11,73 @@ const About = () => {
     <div className='about'>
       <div className="headu">
         <h1 className='heading'>CHANGE <br />THE COURSE</h1>
-      
       </div>
-      
-      <img src={rect} alt="" className='rectangle'/>
-      <div></div>
+
+      <img src={rect} alt="" className='rectangle' />
+
       <div className='im'>
-       
-        <div className='im1'>  <img src={rect2} alt="" className='rect1' />
+        <div className='im1'>
+          <img src={rect2} alt="" className='rect1' />
           <div className='button1'>
-                <ul className='unorder'>
-                  <li > <b>NEW GODDIES</b> <img src={vector} alt=""  className='vect'/><p className="hidden">shop</p> </li>
-                </ul>
-        </div></div>
-      
-        <div className='im2'><img src={rect1} alt="" className='rect2'/>
-           <div className='button2'>
-                   <ul className='unorder'>
-                  <li> <b>PANTRY</b><img src={vector} alt=""  className='vect'/><p className="hidden">shop</p> </li>
-                </ul>
-        </div>
-        </div>
-       <div className='im3'> <img src={rect3} alt="" className='rect3'/>
-         <div className='button3'>
-                 <ul className='unorder'>
-                  <li><b>BATH</b> <img src={vector} alt="" className='vect'/ ><p className="hidden">shop</p></li>
-                </ul>
-        </div>
-       </div>
-      </div>
-   
-       
-      
-      <div className='content'>
-        <div className="bold-text">
-                <h1>WE BELIEVE IN PEOPLE,<br />UNTILL THEY BELIEVE IN<br />  THEMSELVES AGAIN.</h1>
-        </div>
-        <div className="para">
-            <div className="para1">
-                <p> Everything we do is designed to rebuild <br />
-                 self worth and independence, in order to <br />
-                    break free from the cycle of <br />
-                        disadvantage.</p>
-            </div>
-            <div className="para2">
-                 With every purchase you make with us, <br />
-                    you're helping to change the course of <br />
-                        someone's life; you're walking alongside <br />
-                        vulnerable women as they find their way <br />
-                        home again.
-            </div>
-                <a href="" className='link'>SHOP TO SUPPORT</a>
+            <ul className='unorder'>
+              <li>
+                <b>NEW GODDIES</b>
+                <img src={vector} alt="" className='vect' />
+                <p className="hidden">shop</p>
+              </li>
+            </ul>
+          </div>
         </div>
 
+        <div className='im2'>
+          <img src={rect1} alt="" className='rect2' />
+          <div className='button2'>
+            <ul className='unorder'>
+              <li>
+                <b>PANTRY</b>
+                <img src={vector} alt="" className='vect' />
+                <p className="hidden">shop</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+
+        <div className='im3'>
+          <img src={rect3} alt="" className='rect3' />
+          <div className='button3'>
+            <ul className='unorder'>
+              <li>
+                <b>BATH</b>
+                <img src={vector} alt="" className='vect' />
+                <p className="hidden">shop</p>
+              </li>
+            </ul>
+          </div>
+        </div>
+      </div>
+
+      <div className='content'>
+        <div className="bold-text">
+          <h1>WE BELIEVE IN PEOPLE,<br />UNTILL THEY BELIEVE IN<br />THEMSELVES AGAIN.</h1>
+        </div>
+        <div className="para">
+          <div className="para1">
+            <p>
+              Everything we do is designed to rebuild <br />
+              self worth and independence, in order to <br />
+              break free from the cycle of <br />
+              disadvantage.
+            </p>
+          </div>
+          <div className="para2">
+            With every purchase you make with us, <br />
+            you're helping to change the course of <br />
+            someone's life; you're walking alongside <br />
+            vulnerable women as they find their way <br />
+            home again.
+          </div>
+          <a href="#" className='link'>SHOP TO SUPPORT</a>
+        </div>
       </div>
     </div>
   )
